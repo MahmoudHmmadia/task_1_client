33cfad747485e2e18692f797e33401172258304c
Mahmoud Hmmadia <102804429+MahmoudHmmadia@users.noreply.github.com>
2023-10-02 09:27:46 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3500/");
+const socket = io("https://task-1-server.onrender.com/");
 type Message = {
   message: string;
   id: string;
